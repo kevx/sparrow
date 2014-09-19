a5b724e9094e8d20b83e3dfaf6c3a51359e2e873
xiazhen <zhen.xia@duitang.com>
2014-09-19 15:39:15 +0800

    add ssl support

--- src/main/java/org/kevx/sparrow/ServerCore.java
+++ src/main/java/org/kevx/sparrow/ServerCore.java
@@ -2,12 +2,17 @@ package org.kevx.sparrow;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
@@ -19,6 +24,8 @@ import com.sun.net.httpserver.Filter;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
 
 /**
  * @author kevx
@@ -30,17 +37,34 @@ public class ServerCore {
 	private static final Properties handlerConf = new Properties();
 	private static final Properties filterConf = new Properties();
 	
+	private static Map<String, HttpHandler> handlers;
+	private static List<Filter> filters;
+	
 	public static void main(String[] args) {
 		readConf();
-		Map<String, HttpHandler> handlers = initHandlers();
-		List<Filter> filters = initFilters();
+		handlers = initHandlers();
+		filters = initFilters();
 		
+		String[] serverModes = StringUtils.split(serverConf.getProperty("SERVER_MODE"), ',');
+		if (serverModes.length < 1 || serverModes.length > 2) {
+			System.out.println("invalid_server_mode");
+			return;
+		}
+		
+		for (String sm : serverModes) {
+			if ("http".equalsIgnoreCase(sm)) initHttpServer();
+			if ("https".equalsIgnoreCase(sm)) initHttpsServer();
+		}
+		System.out.println("sparrow_started!");
+	}
+
+	static void initHttpServer() {
 		InetSocketAddress addr = new InetSocketAddress(
 			NumberUtils.toInt(serverConf.getProperty("SERVER_CORE_PORT"))
 		);
 		try {
 			HttpServer s = HttpServer.create(addr, 0);
-			Executor exe = new ForkJoinPool();//Executors.newFixedThreadPool(8);
+			Executor exe = new ForkJoinPool();
 			s.setExecutor(exe);
 			for (Entry<String, HttpHandler> e : handlers.entrySet()) {
 				HttpContext hc = s.createContext(e.getKey(), e.getValue());
@@ -51,7 +75,42 @@ public class ServerCore {
 			e.printStackTrace();
 		}
 	}
-
+	
+	static void initHttpsServer() {
+		try {
+			Class<ServerCore> cls = ServerCore.class;
+			char[] passwd = serverConf.getProperty("KEYSTORE_PASSWORD").toCharArray();
+			String certfile = serverConf.getProperty("CERT_FILE");
+			SSLContext sslContext = SSLContext.getInstance("SSLv3");
+			KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+			KeyStore ks = KeyStore.getInstance("JKS");  
+			ks.load(cls.getResourceAsStream(certfile), passwd);
+			kmf.init(ks, passwd);
+			sslContext.init(kmf.getKeyManagers(), null, null);
+			
+			HttpsConfigurator conf = new HttpsConfigurator(sslContext);
+			InetSocketAddress addr = new InetSocketAddress(
+				NumberUtils.toInt(serverConf.getProperty("SERVER_HTTPS_PORT"))
+			);
+			
+			HttpsServer s = HttpsServer.create(addr, 0);
+			int poolsize = NumberUtils.toInt(
+				serverConf.getProperty("POOL_THREADS_COUNT"), 
+				Runtime.getRuntime().availableProcessors() * 4
+			);
+			Executor exe = Executors.newFixedThreadPool(poolsize);
+			s.setExecutor(exe);
+			s.setHttpsConfigurator(conf);
+			for (Entry<String, HttpHandler> e : handlers.entrySet()) {
+				HttpContext hc = s.createContext(e.getKey(), e.getValue());
+				hc.getFilters().addAll(filters);
+			}
+			s.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	static void readConf() {
 		try {
 			Class<ServerCore> cls = ServerCore.class;
